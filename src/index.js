e71280b87c5c264b05cb11e6effb4e3fd050c53c
aleksandra_w <aleksandra.wlodarska94@gmail.com>
2019-09-04 15:27:06 +0200

    no change

--- src/index.js
+++ src/index.js
@@ -14,16 +14,16 @@ const showMeteoData = async () => {
     console.log(meteo)
     // stan na obecny dzień
     document.getElementsByClassName("main__box2--temp header")[0].innerHTML = `${meteo[0].temp}&deg;C`;
-    document.getElementsByClassName("main__box2--minTemp")[0].innerHTML= `temp. min. ${meteo[0].temp_min}&deg;C`;
+    document.getElementsByClassName("main__box2--minTemp")[0].innerHTML = `temp. min. ${meteo[0].temp_min}&deg;C`;
     document.getElementsByClassName("main__box2--maxTemp")[0].innerHTML = `temp. max. ${meteo[0].temp_max}&deg;C`;
     document.getElementsByClassName("pressure")[0].textContent = `${meteo[0].pressure}hPa`;
     document.getElementsByClassName("humidity")[0].textContent = `${meteo[0].humidity}%`;
     document.getElementsByClassName("wind")[0].textContent = `${meteo[0].wind_speed}km/h`;
-    
+
     const information = document.getElementsByClassName("main__box1--paragraph")[0];
     const description = meteo[0].description;
     console.log(description);
-    switch (description){
+    switch (description) {
         case "clear sky":
             information.textContent = "Nie pada!";
             break;
@@ -37,7 +37,7 @@ const showMeteoData = async () => {
             information.textContent = "Może wystąpić lekki deszczyk!";
             break;
         default:
-            information.textContent = description;  
+            information.textContent = description;
     }
 
     //stan na następne 5 dni
@@ -46,16 +46,20 @@ const showMeteoData = async () => {
     console.log(nextDays);
     const nextDaysTempMin = document.querySelectorAll(".rightAside__forcast--minMax > p:first-child");
     const nextDaysTempMax = document.querySelectorAll(".rightAside__forcast--minMax > p:last-child");
-    nextDays.forEach((element, i)=>{
-        let dateElement = new Date(meteo[i+1].date);
+    nextDays.forEach((element, i) => {
+        let dateElement = new Date(meteo[i + 1].date);
         element.innerHTML = days[dateElement.getDay()];
     });
-    nextDaysDate.forEach((element, i)=>{
-        let dateElement = new Date(meteo[i+1].date);
+    nextDaysDate.forEach((element, i) => {
+        let dateElement = new Date(meteo[i + 1].date);
         element.innerHTML = `${dateElement.getDate()} ${month[dateElement.getMonth()]}`;
     });
-    nextDaysTempMin.forEach((element, i) => {element.innerHTML = `temp. min: ${meteo[i+1].temp_min}&deg;C`;});
-    nextDaysTempMax.forEach((element, i) => {element.innerHTML = `temp. max: ${meteo[i+1].temp_max}&deg;C`;});
+    nextDaysTempMin.forEach((element, i) => {
+        element.innerHTML = `temp. min: ${meteo[i+1].temp_min}&deg;C`;
+    });
+    nextDaysTempMax.forEach((element, i) => {
+        element.innerHTML = `temp. max: ${meteo[i+1].temp_max}&deg;C`;
+    });
 }
 
 
